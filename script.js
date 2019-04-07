829d5f20b77c1c4745a2be826dd10536631901cb
Annu Shokeen <shokeen.annu@gmail.com>
2019-04-07 11:38:58 +1000

    Update script.js
    scroll to a div id is done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-//document.getElementById("search").onclick(showPage());
+
 function showPage() {
-    $('html', 'body').animate({ scrollTop: $("#profiles").position().top });
+    $(window).scrollTop($("#profiles").offset().top);
 }
